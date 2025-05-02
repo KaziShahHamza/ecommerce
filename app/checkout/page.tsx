0c1397e76be84d30498dd1e0bae3eea4484c4fcb
soulfulDreamer <kazishahhamza@outlook.com>
2025-05-02 15:36:48 +0600

    complete project

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,68 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useCartStore } from "@/store/CartStore";
+import { checkoutAction } from "./checkoutAction";
+
 export default function Checkout() {
+  const { items, addItem, removeItem } = useCartStore();
+  const total = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  if (total == 0 || items.length == 0) {
     return (
-        <div>Checkout Page</div>
-    )
-} 
+      <div className="container mx-auto px-4 py-8 text-center">
+        <h1 className="text-3xl font-bold mb-4">Cart is empty</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Checkout</h1>
+      <Card className="max-w-md mx-auto mb-8">
+        <CardHeader>
+          <CardTitle className="text-xl font-bold">Order Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {items.map((item) => (
+              <li key={item.id} className="flex flex-col gap-2 border-b pb-2">
+                <div className="flex justify-between">
+                  <span className="font-medium">{item.name}</span>
+                  <span className="font-semibold">
+                    ${((item.price * item.quantity) / 100).toFixed(2)}
+                  </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Button onClick={() => removeItem(item.id)} variant="outline">
+                    -
+                  </Button>
+                  <span className="text-lg font-semibold">{item.quantity}</span>
+                  <Button
+                    onClick={() => addItem({ ...item, quantity: 1 })}
+                    variant="outline"
+                  >
+                    +
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <div className="mt-4 border-t pt-2 text-lg font-semibold">
+            Total: ${(total / 100).toFixed(2)}
+          </div>
+        </CardContent>
+      </Card>
+      <form action={checkoutAction} className="max-w-md mx-auto">
+        <input type="hidden" name="items" value={JSON.stringify(items)} />
+        <Button type="submit" variant="default" className="w-full">
+          Proceed to Payment
+        </Button>
+      </form>
+    </div>
+  );
+}
