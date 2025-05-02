1fae0d437c3343cd7eb9f77947991ebc17a08da5
soulfulDreamer <kazishahhamza@outlook.com>
2025-05-02 12:16:02 +0600

    Add cart funcs

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,7 +13,7 @@ const ProductCard = ({ product }: Props) => {
   const price = product.default_price as Stripe.Price;
 
   return (
-    <Link href={"/products/1"}>
+    <Link href={`/products/${product.id}`}>
       <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-gray-300 gap-0">
         {product.images && product.images[0] && (
           <div className="relative h-80 w-full">
